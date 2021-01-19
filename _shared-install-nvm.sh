8eabdc060150004c0896e4b4d00fae49485a8689
Hernan Silva <hachesilva@gmail.com>
2021-01-19 15:26:09 -0500

    Reorder nvm installation on mac. Add CLI essentials to nvm installer

--- _shared-install-nvm.sh
+++ _shared-install-nvm.sh
@@ -3,3 +3,12 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 nvm install --lts
 nvm use --lts
+
+# Install...
+    # CLI Essentials
+    npm install -g http-server
+    npm install -g live-server
+    npm install -g servor
+    npm install -g sass
+    npm install -g gulp
+    npm install -g yarn
