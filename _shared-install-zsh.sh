0761672d80bb74d40ce8416ce618ea111dd428e0
Hernan Silva <hachesilva@gmail.com>
2021-07-22 14:07:41 -0500

    Remove powerlevel9k from zsh themes

--- _shared-install-zsh.sh
+++ _shared-install-zsh.sh
@@ -6,14 +6,13 @@
 
     # zsh themes
     mkdir -p ~/.oh-my-zsh/custom/themes
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
     git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
     # zsh plugins
     mkdir -p ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
     git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
-    git clone https://github.com/zpm-zsh/undollar ~/.oh-my-zsh/custom/plugins/undollar 
+    git clone https://github.com/zpm-zsh/undollar ~/.oh-my-zsh/custom/plugins/undollar
     git clone https://github.com/zpm-zsh/ls.git ~/.oh-my-zsh/custom/plugins/ls
     git clone https://github.com/zpm-zsh/material-colors ~/.oh-my-zsh/custom/plugins/material-colors
     git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
