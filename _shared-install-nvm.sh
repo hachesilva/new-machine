1e1f29ac4a68db68115e9947352b9ded2c93e7b6
Hernan Silva <hachesilva@gmail.com>
2023-05-21 14:04:18 -0500

    Update nvm installation script. Add node 16 and 18 installation steps

--- _shared-install-nvm.sh
+++ _shared-install-nvm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+nvm install 16
+nvm install 18
 nvm install --lts
 nvm use --lts
 
