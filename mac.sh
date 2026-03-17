4b9d7c044a3899a489799dea49b911c6f40db35a
Hernan Silva <hachesilva@gmail.com>
2026-03-16 21:45:00 -0500

    New apps added to mac.sh

--- mac.sh
+++ mac.sh
@@ -25,9 +25,16 @@ brew install -q zsh
     . _shared-install-nvm.sh
 
     # System essentials
+    brew install -q fd # better find
     brew install -q git
-    brew install -q yt-dlp
+    brew install -q lsd # better ls
     brew install -q mas
+    brew install -q organize-tool # rules-based file manager
+    brew install -q tldr # better man
+    brew install -q yank # text extraction from CLI output
+    brew install -q yazi # CLI file manager
+    brew install -q yt-dlp
+
 
     # Internet
     brew install -q --cask dropbox
@@ -38,9 +45,14 @@ brew install -q zsh
     brew install -q --cask teamviewer
 
     # Media
+    brew install -q --cask calibre
+    brew install -q --cask foobar2000
+    brew install -q --cask handbrake-app
+    brew install -q --cask imageoptim
+    brew install -q --cask petrichor # music player
     brew install -q --cask vlc
-    brew install -q --cask vox
-    brew install -q --cask xnviewmp
+    brew install -q --cask vox # music player
+    brew install -q --cask xnviewmp # image viewer and manager
 
     # Dev
     brew install -q --cask beyond-compare
@@ -57,16 +69,30 @@ brew install -q zsh
     brew install -q --cask visual-studio-code
 
     # Utils
+    brew install -q --cask aldente # battery manager
     brew install -q --cask alt-tab
     brew install -q --cask betterzip
+    brew install -q --cask commander-one # file manager
     brew install -q --cask copyq
+    brew install -q --cask dropzone
     brew install -q --cask espanso
-    brew install -q --cask flux
+    brew install -q --cask finicky # default browser manager
+    brew install -q --cask flux-app
+    brew install -q --cask handy # speech to text
+    brew install -q --cask hyperkey
     brew install -q --cask itsycal
-    brew install -q --cask maccy
-    brew install -q --cask pika
+    brew install -q --cask jordanbaird-ice # alt to bartender
+    brew install -q --cask keka # zip archiver
+    brew install -q --cask linearmouse # better mouse settings
+    brew install -q --cask localsend
+    brew install -q --cask maccy # alt to copyq
+    brew install -q --cask marta # file manager
+    brew install -q --cask notunes # block itunes from opening
+    brew install -q --cask pika # color picker
     brew install -q --cask raycast
-    brew install -q --cask spaceid
+    brew install -q --cask rectangle
+    brew install -q --cask shottr
+    brew install -q --cask spaceid # virtual desktops indicator
     brew install -q --cask veracrypt
     brew install -q --cask vnc-server
     brew install -q --cask vnc-viewer
